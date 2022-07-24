f8b7cee98a945fe58986957df9c0075a9d6ce970
jcole <judc@fulldev.com>
2022-07-24 14:11:29 -0500

    Add comments

--- twitter-api/src/main/java/com/judcole/twitter/api/SpringDocOpenAPIConfig.java
+++ twitter-api/src/main/java/com/judcole/twitter/api/SpringDocOpenAPIConfig.java
@@ -4,10 +4,19 @@ import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * The class to configure SpringDoc OpenAPI (Swagger).
+ */
 @Configuration
 public class SpringDocOpenAPIConfig {
+    /**
+     * Get the public API definitions.
+     *
+     * @return the API definitions
+     */
     @Bean
     public GroupedOpenApi publicApi() {
+        // Primary group with all matching API calls
         return GroupedOpenApi.builder()
                 .group("primary")
                 .pathsToMatch("/**")
